daa20f93941a2b8219931bac2a161c63a51df4c3
business@satotakuya.jp <business@satotakuya.jp>
2018-10-19 14:05:59 +0900

    index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const electron = require("electron")
 const {app, BrowserWindow, autoUpdater, dialog, Menu, protocol, ipcMain, Tray} = electron
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
-process.env['GH_TOKEN'] = "2cc6ee1dfdcd8b8b9fc9b3821fbb8e8560115cb5";
+process.env['GH_TOKEN'] = "98e0f3352dee20b0642e1197f547695b34c3ae8b";
 
 const log = require('electron-log');
 const path = require("path");
